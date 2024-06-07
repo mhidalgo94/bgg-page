125284d75a22f70b08331202deaf2286eacf6e91
Mario Sergio <marito@Marito-Macbook-Pro.hsd1.fl.comcast.net>
2024-06-07 01:40:34 -0400

    Add animation to OurImpact

--- frontend/src/component/Sections/OurImpact/OurImpact.jsx
+++ frontend/src/component/Sections/OurImpact/OurImpact.jsx
@@ -1,9 +1,74 @@
 import './styles.css';
-// import DonateBtn from './DonateBtn';
-import imgBackground from '../../../assets/images/back-impact.jpg'
+import imgBackground from '../../../assets/images/back-impact.jpg';
 import SmokingRoomsIcon from '@mui/icons-material/SmokingRooms';
 import QueryBuilderIcon from '@mui/icons-material/QueryBuilder';
 import RecyclingIcon from '@mui/icons-material/Recycling';
+import {motion} from 'framer-motion';
+
+// Var for animation
+const text = {
+    initial :{
+        left: '-400px',
+        position: 'relative',
+        opacity: 0
+    },
+    animate:{
+        left: '0px',   
+        opacity: 1,
+        transition: {
+            type: "easy",
+            duration: .8,
+            delay: 0.4,
+
+        }
+    }
+}
+
+const motImage = {
+    initial :{
+        scale: 1.6,
+        position: 'relative',
+        opacity: 0
+    },
+    animate:{
+        scale: 1,   
+        opacity: 1,
+        transition: {
+            type: "easy",
+            duration: .8,
+            delay: 0.4,
+
+        }
+    }
+}
+
+
+const card = {
+    initial :{
+        scale: .5,
+        opacity: 0
+    },
+    animate:{  
+        top: '0px',
+        scale: 1,
+        opacity: 1,
+        transition: {
+            duration: .5,
+            delay: 0.5,
+            // ease: [0, 0.71, 0.2, 1.01]
+        }
+    },
+    hover:{
+        scale: 1.04, 
+        transition:{
+            type: "spring",
+            // delay: 0,
+
+        }
+    }
+}
+
+
 
 export default function OurImpact() {
   return (
@@ -11,21 +76,21 @@ export default function OurImpact() {
     <div className='section-our-impact'>
         <div className='section-container'>
             <div className="our-impact-title">
-                <p className='txt-1'>Our achievements</p>
-                <h1 className='impact-title'>Our Impact</h1>
-                <p className='impact-txt'>
+                <motion.p className='txt-1' initial={text.initial} whileInView={text.animate} >Our achievements</motion.p>
+                <motion.h1 className='impact-title' initial={text.initial} whileInView={text.animate} >Our Impact</motion.h1>
+                <motion.p className='impact-txt' initial={text.initial} whileInView={text.animate} >
                 Beaches Go Green raises awareness about plastic waste's impact on oceans and the environment. Through education and community engagement, they inspire sustainable practices and reduce single-use plastics to protect natural resources for future generations
-                </p>
+                </motion.p>
                 {/* <div style={{marginTop:'30px'}}>
                     <DonateBtn />
                 </div> */}
             </div>
             <div className='our-impact-right'>
                 <div className='img-container'>
-                    <img src={imgBackground} alt="" />
+                    <motion.img src={imgBackground} alt="Background image Our Impact" initial={motImage.initial} whileInView={motImage.animate}/>
                 </div>
                 <div className='container-cards-impact'>
-                    <div className='card-info'>
+                    <motion.div className='card-info' initial={card.initial} whileInView={card.animate}whileHover={card.hover}>
                         <div className='card-icons'>
                             <SmokingRoomsIcon  sx={{ fontSize: 40,color: "var(--dark-orange)" }}/>
                         </div>
@@ -33,8 +98,8 @@ export default function OurImpact() {
                             <h4 style={{color:'var(--dark-orange)'}}>158,283 <span style={{color:"var(--dark-orange)"}}>*</span></h4>
                             <p>Cigarette buds collected and recycled with TerraCycle</p>
                         </div>
-                    </div>
-                    <div className='card-info'>
+                    </motion.div>
+                    <motion.div className='card-info' initial={card.initial} whileInView={card.animate} whileHover={card.hover}>
                         <div className='card-icons'>
                             <QueryBuilderIcon  sx={{ fontSize: 40,color: "var(--primary-blue)" }}/>
                         </div>
@@ -42,16 +107,16 @@ export default function OurImpact() {
                             <h4 style={{color:"var(--primary-blue)"}}>27,482<span style={{color:"var(--primary-blue)"}}>*</span></h4>
                             <p>Volunteer hours, not including operations team volunteer hours</p>
                         </div>
-                    </div>
-                    <div className='card-info'>
-                    <div className='card-icons'>
+                    </motion.div>
+                    <motion.div className='card-info' initial={card.initial} whileInView={card.animate} whileHover={card.hover}>
+                        <div className='card-icons'>
                             <RecyclingIcon  sx={{ fontSize: 40,color: "var(--accent)" }}/>
                         </div>
                         <div className="card-details">
                             <h4 style={{color:"var(--accent)"}}>21,601<span style={{color:"var(--accent)"}}>*</span></h4>
                             <p>Pounds of trash collected</p>
                         </div>
-                    </div>
+                    </motion.div>
 
                 </div>
             </div>
